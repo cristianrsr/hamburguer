8dde183718b791d8ae784c457710345fb51025a4
marcoscesarse <marcos.cesar.se@gmail.com>
2026-04-09 17:07:46 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import categoriaRoutes from './routes/categoriaRoutes.js';
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use('/categoria',categoriaRoutes);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ migrations/20260409195523-create-entregas.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('entregas', {
+      pedido_id :{
+        type : Sequelize.INTEGER,
+        allowNull : false,
+        primaryKey : true
+      },
+      codigo_rastreio:{
+        type: Sequelize.STRING,
+        allowNull : false,
+        unique : true
+      },
+      endereco :{
+        type: Sequelize.STRING,
+        allowNull : false
+      },
+      createdAt: { type: Sequelize.DATE },
+      updatedAt: { type: Sequelize.DATE },
+      deletedAt: { type: Sequelize.DATE }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('entregas');
+  }
+};
